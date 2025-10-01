77619c18c8e94ec70811677522b0c802c1a56e5c
Jay Yoon <148425666+jayyoonakajaeha@users.noreply.github.com>
2025-10-02 06:55:06 +0900

    Add files via upload

---
+++ check_token.py
@@ -0,0 +1,6 @@
+import json
+import numpy as np
+def check(path='vocab.json'):
+    with open(path,'r') as f:
+        for k,v in json.load(f).items():
+            print(f'{eval(k)} -> {v} -> {bytes(v).decode('utf-8',errors='replace')}')
