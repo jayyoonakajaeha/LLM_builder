1f66e09e00c90fe7fb2423bbf1654783805bcf63
Jay Yoon <148425666+jayyoonakajaeha@users.noreply.github.com>
2025-10-03 16:24:59 +0900

    Update check_token.py

--- check_token.py
+++ check_token.py
@@ -3,4 +3,5 @@ import numpy as np
 def check(path='vocab.json'):
     with open(path,'r') as f:
         for k,v in json.load(f).items():
-            print(f'{eval(k)} -> {v} -> {bytes(v).decode('utf-8',errors='replace')}')
+            print(f"{eval(k)} -> {v} -> {bytes(v).decode('utf-8',errors='replace')}")
+
