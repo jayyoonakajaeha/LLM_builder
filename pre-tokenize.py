ebccda4690cfed1740591ee1f3e09386c34c33d2
Jay Yoon <148425666+jayyoonakajaeha@users.noreply.github.com>
2025-10-03 16:12:04 +0900

    Update pre-tokenize.py

--- pre-tokenize.py
+++ pre-tokenize.py
@@ -1,4 +1,11 @@
 import numpy as np
+import json
+
+def jsonl2txt(path='wiki_00.jsonl', out_path='wiki_00.txt'):
+    with open(path,'r') as f, open(out_path,'w') as out:
+        for line in f:
+            data = json.loads(line)
+            out.write(data['text'] + '<doc>')
 
 def txt2bin(chunk_size=1000000000):
     with open('kowiki_corpus.txt','rb') as f, open('kowiki_corpus.bin','wb') as out:
@@ -9,10 +16,8 @@ def txt2bin(chunk_size=1000000000):
 
             token = np.frombuffer(data, dtype=np.uint8).astype(np.uint32)
             token.tofile(out)
-    f.close()
-    out.close()
             
 def check_bin(chunk_size=2000):
     with open('kowiki_corpus.bin','rb') as f:
         print(bytes(np.frombuffer(f.read(chunk_size),dtype=np.uint32).astype(np.uint8)).decode('utf-8',errors='replace'))
-    f.close()
+
